339aac7874ea75ba51256aedeef282d141c0a3b1
Carey Hinoki <carey.r.hinoki.-nd@disney.com>
2016-08-29 17:10:08 -0700

    refactor(videojs-chapter-thumbnail.spec): Eslint update.

--- test/videojs-chapter-thumbnail.spec.js
+++ test/videojs-chapter-thumbnail.spec.js
@@ -1,68 +1,68 @@
+/* global videojs */
+
 import 'expose?videojs!video.js';
 
 import ChapterThumbnails from '../src/videojs-chapter-thumbnail';
 
 import TRACK_ID from '../src/track/text-track';
 
-describe('chapter-thumbnail.js', function () {
-    var player;
+describe('chapter-thumbnail.js', () => {
+  let player;
 
-    beforeEach(function () {
-        // followed example — https://github.com/videojs/videojs-contrib-ads
+  beforeEach(() => {
+    // followed example — https://github.com/videojs/videojs-contrib-ads
 
-        videojs.getComponent('Html5').isSupported = function () {
-            return true;
-        };
+    videojs.getComponent('Html5').isSupported = () => true;
 
-        delete videojs.getComponent('Html5').prototype.setSource;
+    delete videojs.getComponent('Html5').prototype.setSource;
 
-        let video = document.createElement('video');
+    const video = document.createElement('video');
 
-        video.load = function () {};
-        video.play = function () {};
+    video.load = () => {};
+    video.play = () => {};
 
-        document.getElementById('video_fixture').innerHTML = video.outerHTML;
+    document.getElementById('video_fixture').innerHTML = video.outerHTML;
 
-        player = videojs(video);
+    player = videojs(video);
+  });
+
+  it('should initialize with defaults.', () => {
+    const chapterThumbnail = new ChapterThumbnails(player);
+
+    chapterThumbnail.addTextTrack();
+
+    const track = player.textTracks().getTrackById(TRACK_ID);
+
+    expect(track.id).toBe(TRACK_ID);
+    expect(track.kind).toBe('metadata');
+    expect(track.label).toBe('English');
+    expect(track.language).toBe('en');
+    expect(track.mode).toBe('hidden');
+  });
+
+  it('should initialize with options.', () => {
+    const chapterThumbnail = new ChapterThumbnails(player, {
+      label: 'French',
+      language: 'fr',
     });
 
-    it('should initialize with defaults.', function () {
-        let chapter_thumbnail = new ChapterThumbnails(player);
+    chapterThumbnail.addTextTrack();
 
-        chapter_thumbnail.addTextTrack();
+    const track = player.textTracks().getTrackById(TRACK_ID);
 
-        let track = player.textTracks().getTrackById(TRACK_ID);
+    expect(track.label).toBe('French');
+    expect(track.language).toBe('fr');
+  });
 
-        expect(track.id).toBe(TRACK_ID);
-        expect(track.kind).toBe('metadata');
-        expect(track.label).toBe('English');
-        expect(track.language).toBe('en');
-        expect(track.mode).toBe('hidden');
-    });
+  it('should contain one textTrack', () => {
+    expect(player.textTracks().length).toBe(0);
 
-    it('should initialize with options.', function () {
-        let chapter_thumbnail = new ChapterThumbnails(player, {
-            label: 'French',
-            language: 'fr'
-        });
+    const chapterThumbnail = new ChapterThumbnails(player);
 
-        chapter_thumbnail.addTextTrack();
+    chapterThumbnail.addTextTrack();
 
-        let track = player.textTracks().getTrackById(TRACK_ID);
+    expect(player.textTracks().length).toBe(1);
+  });
 
-        expect(track.label).toBe('French');
-        expect(track.language).toBe('fr');
-    });
-
-    it('should contain one textTrack', function () {
-        expect(player.textTracks().length).toBe(0);
-
-        let chapter_thumbnail = new ChapterThumbnails(player);
-
-        chapter_thumbnail.addTextTrack();
-
-        expect(player.textTracks().length).toBe(1);
-    });
-
-    // TODO: no idea how to do other tests…
+  // TODO: no idea how to do other tests…
 });
